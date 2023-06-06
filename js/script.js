1c00a1b0b181660ad127bac2fece184f80dbd696
gabriel-belo <rm551669@fiap.com.br>
2023-06-06 14:54:03 -0300

    boas vindas

--- js/script.js
+++ js/script.js
@@ -2,3 +2,6 @@ const checkbox = document.querySelector("#checkbox");
 checkbox.addEventListener("change", () => {
   document.body.classList.toggle("dark");
 });
+window.onload = function() {
+  alert("Seja bem-vindo!");
+};
