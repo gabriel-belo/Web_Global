f5b34be24fed0daf26e4ce4501e14ea9371e16c8
gabriel-belo <rm551669@fiap.com.br>
2023-06-06 12:20:07 -0300

    dados

--- js/script.js
+++ js/script.js
@@ -6,3 +6,10 @@ checkbox.addEventListener("change", () => {
 window.onload = function() {
   alert("Seja bem-vindo!");
 };
+
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    var name = document.getElementById("name").value;
+    var phone = document.getElementById("phone").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+  
