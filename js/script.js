7981935dac51edf24de037ed038094c0b2c8b1bf
gabriel-belo <rm551669@fiap.com.br>
2023-06-06 14:56:46 -0300

    função login

--- js/script.js
+++ js/script.js
@@ -16,4 +16,15 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
     event.preventDefault();
   }
 });
+function login() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if (username === "Admin" && password === "123456") {
+    window.location.href = "https://www.unicef.org/brazil/comunicados-de-imprensa/relatorio-da-onu-numeros-globais-de-fome-subiram-para-cerca-de-828-milhoes-em-2021";
+  } else {
+    alert("Usuário ou senha inválidos!");
+  }
+}
+
 
